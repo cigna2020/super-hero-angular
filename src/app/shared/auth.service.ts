1390d11ee934aad162634aa5e2fb1536ef7f4b43
Alex <cigan@ua.fm>
2021-05-06 19:26:27 +0300

    US002, create auth service with methods to login and logout etc

---
+++ src/app/shared/auth.service.ts
@@ -0,0 +1,47 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AuthResponse, User} from './interface';
+import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
+import {tap} from 'rxjs/operators';
+
+@Injectable({providedIn: 'root'})
+
+export class AuthService {
+
+  constructor(private http: HttpClient) {}
+
+  get token(): string {
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+    return localStorage.getItem('fb-token');
+  }
+
+  login(user: User): Observable<any> {
+    user.returnSecureToken = true;
+    return  this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+     .pipe(tap(this.setToken));
+  }
+
+  logout(): void {
+    this.setToken(null);
+
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.token;
+  }
+
+  private setToken(response: AuthResponse | null): void {
+    if (response) {
+      const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000);
+      localStorage.setItem('fb-token', response.idToken);
+      localStorage.setItem('fb-token-exp', expDate.toString());
+    } else  {
+      localStorage.clear();
+    }
+  }
+}
